e1c508a89c6a0cc1beead80d01cf2b9789ce3722
bolt-art <83206210+bolt-art@users.noreply.github.com>
2023-02-24 09:37:22 +0100

    Create spark_operator_in_cluster.py

---
+++ spark_operator_in_cluster.py
@@ -0,0 +1,33 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.utils.dates import days_ago
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'max_active_runs': 1,
+    'retries': 0
+}
+dag = DAG(
+    dag_id="spark_operator_in_cluster",
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    tags=['pod','inside','spark-operator'],
+    template_searchpath=["/usr/local/spark/spark-operator/"]
+)
+spark_operator = SparkKubernetesOperator(
+    task_id='spark_pi_submit',
+    namespace='operators',
+    application_file="spark_application_1.yaml",
+    kubernetes_conn_id="kubernetes_default",
+    dag=dag,
+    api_group="sparkoperator.k8s.io"
+)
+start = DummyOperator(task_id="start", dag=dag)
+end = DummyOperator(task_id="end", dag=dag)
+start >> spark_operator >> end
