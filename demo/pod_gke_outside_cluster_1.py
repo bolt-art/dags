464a69300791f3a90df8af2a54a90d1371ceb4ae
bolt-art <83206210+bolt-art@users.noreply.github.com>
2023-02-27 13:05:10 +0100

    Update pod_gke_outside_cluster_1.py

--- demo/pod_gke_outside_cluster_1.py
+++ demo/pod_gke_outside_cluster_1.py
@@ -42,4 +42,6 @@ start_pod = GKEStartPodOperator(
     project_id="artur-bolt-development",
     dag=dag,
     delegate=delegated_credentials,
+    gcp_conn_id='google_cloud_default',
+    extra_envs={"GOOGLE_APPLICATION_CREDENTIALS": "/usr/local/google/service_account.json"}
 )
