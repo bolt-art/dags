764cae408120474923f9b7bbbaffe5ca62ff5484
bolt-art <83206210+bolt-art@users.noreply.github.com>
2023-02-27 13:24:26 +0100

    Update pod_gke_outside_cluster_1.py

--- demo/pod_gke_outside_cluster_1.py
+++ demo/pod_gke_outside_cluster_1.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.contrib.operators.gcp_container_operator import GKEPodOperator
+from airflow.providers.google.cloud.operators.kubernetes_engine import GKEStartPodOperator
 from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
 from google.oauth2 import service_account
 from datetime import datetime, timedelta
@@ -27,16 +27,20 @@ credentials = service_account.Credentials.from_service_account_file(
     scopes=['https://www.googleapis.com/auth/cloud-platform']
 )
 delegated_credentials = credentials.with_subject('airflow-identity@artur-bolt-development.iam.gserviceaccount.com')
-start_pod = GKEPodOperator(
-    namespace="operators",
+start_pod = GKEStartPodOperator(
+    namespace="infra",
     image="bitnami/kubectl",
     cmds=["sh", "-c", "kubectl exec -ti --namespace operators spark-master-0 -- spark-submit --master spark://spark-master-svc.operators.svc.cluster.local:7077 --class org.apache.spark.examples.SparkPi https://artur-bolt-spark-jars.s3.eu-central-1.amazonaws.com/spark-examples_2.12-3.3.2.jar 1000"],
     name="kubectl-pod",
-    task_id='start-kubectl-pod',
-    project_id='artur-bolt-development',
+    do_xcom_push=False,
+    is_delete_operator_pod=True,
+    task_id="start-kubectl-pod",
+    get_logs=True,        
     location="europe-central2",
-    cluster_name='target-double',
-    gcp_conn_id='google_cloud_sa',
+    cluster_name="k8s-development",
+    in_cluster=False,
+    project_id="artur-bolt-development",
+    delegated_credentials=delegated_credentials,
     dag=dag
 )
 start_pod
