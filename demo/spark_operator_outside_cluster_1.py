725c270d694daf554a054e990fedec980d81f841
bolt-art <83206210+bolt-art@users.noreply.github.com>
2023-02-27 12:55:04 +0100

    Update spark_operator_outside_cluster_1.py

--- demo/spark_operator_outside_cluster_1.py
+++ demo/spark_operator_outside_cluster_1.py
@@ -2,6 +2,8 @@ from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
+from google.oauth2 import service_account
 from airflow.utils.dates import days_ago
 default_args = {
     'owner': 'spark-operator',
@@ -20,11 +22,18 @@ dag = DAG(
     tags=['pod','outside','spark-operator'],
     template_searchpath=["/usr/local/spark/spark-operator/"]
 )
+gcp_hook = GoogleCloudBaseHook(gcp_conn_id='google_cloud_default')
+credentials = service_account.Credentials.from_service_account_file(
+    '/usr/local/google/service_account.json',
+    scopes=['https://www.googleapis.com/auth/cloud-platform']
+)
+delegated_credentials = credentials.with_subject('airflow-identity@artur-bolt-development.iam.gserviceaccount.com')
 spark_operator = SparkKubernetesOperator(
     task_id='spark_pi_submit',
     namespace='operators',
     application_file="spark_application_1.yaml",
-    kubernetes_conn_id='kubernetes_target',
+    #kubernetes_conn_id='kubernetes_target',
+    delegate=delegated_credentials,
     dag=dag,
     api_group='sparkoperator.k8s.io'
 )
@@ -32,7 +41,8 @@ sensor = SparkKubernetesSensor(
     task_id='spark_pi_submit_sensor',
     namespace="operators",
     application_name="{{ task_instance.xcom_pull(task_ids='spark_pi_submit')['metadata']['name'] }}",
-    kubernetes_conn_id="kubernetes_target",
+    #kubernetes_conn_id="kubernetes_target",
+    delegate=delegated_credentials,
     dag=dag,
     api_group="sparkoperator.k8s.io",
     attach_log=True
